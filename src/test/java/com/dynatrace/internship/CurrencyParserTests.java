1092441a340f8c54fa7a8753f303500b88b8e788
Sladamos <sladamo@wp.pl>
2023-04-22 09:40:38 +0200

    Currency Parser tests

--- src/test/java/com/dynatrace/internship/CurrencyParserTests.java
+++ src/test/java/com/dynatrace/internship/CurrencyParserTests.java
@@ -1,18 +1,26 @@
 package com.dynatrace.internship;
 
+import com.dynatrace.internship.parsers.CurrencyParser;
 import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CurrencyParserTests {
 
     @Test
     public void CorrectCode()
     {
-
+        CurrencyParser parser = new CurrencyParserImpl();
+        Currency instance = parser.getCurrencyInstance("GBP");
+        assertSame(Currency.getInstance("GBP"), instance);
     }
 
     @Test
     public void IncorrectCode()
     {
-
+        CurrencyParser parser = new CurrencyParserImpl();
+        assertThrows(IncorrectCodeError, parser.getCurrencyInstance("Incorrect code"));
     }
 }
