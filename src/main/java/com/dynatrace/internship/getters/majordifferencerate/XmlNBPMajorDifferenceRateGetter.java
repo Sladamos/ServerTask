e4707d6b8e5608ecde07e4154799484a489f2721
Sladamos <sladamo@wp.pl>
2023-04-23 14:12:28 +0200

    Change device , pseudocode

--- src/main/java/com/dynatrace/internship/getters/majordifferencerate/XmlNBPMajorDifferenceRateGetter.java
+++ src/main/java/com/dynatrace/internship/getters/majordifferencerate/XmlNBPMajorDifferenceRateGetter.java
@@ -1,8 +1,15 @@
 package com.dynatrace.internship.getters.majordifferencerate;
 
 import com.dynatrace.internship.structures.RateDifference;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 import java.net.URL;
+import java.time.LocalDate;
 
 public class XmlNBPMajorDifferenceRateGetter extends NBPMajorDifferenceRateGetter {
     public XmlNBPMajorDifferenceRateGetter(char tableId) {
@@ -16,6 +23,22 @@ public class XmlNBPMajorDifferenceRateGetter extends NBPMajorDifferenceRateGette
 
     @Override
     protected RateDifference getMajorDifferenceFromUrl(URL url) {
-        return null;
+        try {
+            XPathFactory xpf = XPathFactory.newInstance();
+            XPath xPath = xpf.newXPath();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(url.openStream());
+            //get nodes from document
+            //for each -> select node with biggest difference
+            //return RateDifference
+            LocalDate differenceDate;
+            double differenceValue;
+            return new RateDifference(differenceDate, differenceValue);
+            return Double.parseDouble(value);
+        }
+        catch (Exception err) {
+            throw new RuntimeException(err);
+        }
     }
 }
