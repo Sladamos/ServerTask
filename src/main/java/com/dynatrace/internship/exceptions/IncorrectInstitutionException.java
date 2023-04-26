7b84b19d1411a353991e2f04d33d7435e7afa976
Sladamos <sladamo@wp.pl>
2023-04-26 17:07:37 +0200

    Better Average Rates controller

---
+++ src/main/java/com/dynatrace/internship/exceptions/IncorrectInstitutionException.java
@@ -0,0 +1,7 @@
+package com.dynatrace.internship.exceptions;
+
+public class IncorrectInstitutionException extends RuntimeException {
+	public IncorrectInstitutionException(String message) {
+		super(message);
+	}
+}
